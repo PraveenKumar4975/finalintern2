d84527f00ab7a6ddd31bcbb3cabc25d5e2778818
PRAVEEN KUMAR B <146192831+PraveenKumar4975@users.noreply.github.com>
2025-07-07 01:10:59 +0530

    Update Navbarwithfilter.jsx
    change the navbrwithfilter width increse

--- src/components/Navbarwithfilter.jsx
+++ src/components/Navbarwithfilter.jsx
@@ -6,8 +6,8 @@ import { Job_type } from './Job_type'
 import Location  from './Location'
 export const Navbarwithfilter = () => {
   return (
-    <div><div className='relative w-full shadow-[0px_0px_15px_10px_#7F7F7F26]'>
-            <div className='absolute z-50 top-[21px] pl-[275px]'>
+    <div><div className='relative w-[1580px] shadow-[0px_0px_15px_10px_#7F7F7F26]'>
+            <div className='absolute z-50 top-[21px] pl-[355px]'>
                  <Navbars/>
             </div>
             <div className="relative w-full h-[214px] px-6 py-4 bg-white shadow-[0px_4px_8px_#C6BFBF40] shadow-sm rounded-xl">
